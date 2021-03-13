6e6e0ce3747d241c452b8a1e16cf5d5b29db5c3e
joseantoniovegaruiz <joseantonio.vegaruiz@gmail.com>
2021-03-13 11:23:21 -0600

    init

---
+++ src/main/java/com/galvanize/RestSpring/RestSpringApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.RestSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSpringApplication.class, args);
+	}
+
+}
